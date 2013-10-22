df701f997c5c0409736e55ca5add1911d58f57bc
pingortle <pingortle@gmail.com>
2013-10-22 15:03:24 -0500

    Initial ticket creation UI.

--- AdmitOne/AdmitOne/ViewModel/CreateTicketsViewModel.cs
+++ AdmitOne/AdmitOne/ViewModel/CreateTicketsViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace AdmitOne.ViewModel
@@ -13,7 +14,31 @@ namespace AdmitOne.ViewModel
             HostScreen = screen ?? new DefaultScreen(RxApp.DependencyResolver);
 
             GoBack = HostScreen.Router.NavigateBack;
+
+            CurrentBatch = new ReactiveList<TicketItemViewModel>();
+
+            AddTicket = new ReactiveCommand(
+                this.WhenAny(
+                x => x.CurrentBatch,
+                y => y.TicketText,
+                (x, y) => x.Value.Count() <= 100 && !string.IsNullOrWhiteSpace(y.Value)));
+            AddTicket.Subscribe(_ =>
+                {
+                    CurrentBatch.Add(new TicketItemViewModel(TicketText, CurrentBatch.Remove));
+                    TicketText = default(string);
+                });
         }
+
+        public IReactiveCommand AddTicket { get; private set; }
+
+        private string _ticketText;
+        public string TicketText
+        {
+            get { return _ticketText; }
+            set { this.RaiseAndSetIfChanged(ref _ticketText, value); }
+        }
+
+        public IReactiveList<TicketItemViewModel> CurrentBatch { get; private set; }
 
         public IReactiveCommand GoBack { get; private set; }
 
---
+++ AdmitOne/AdmitOne/ViewModel/TicketItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmitOne.ViewModel
+{
+    public class TicketItemViewModel: ReactiveObject
+    {
+        public TicketItemViewModel(string text, Action<object> removeAction = null)
+        {
+            removeAction = removeAction ?? (x => Console.WriteLine("NoOp."));
+            Text = text;
+
+            RemoveItem = new ReactiveCommand();
+            RemoveItem.Subscribe(removeAction);
+        }
+
+        public string Text { get; private set; }
+
+        public IReactiveCommand RemoveItem { get; private set; }
+    }
+}
