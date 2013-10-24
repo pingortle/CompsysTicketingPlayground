e3165d7bb5166a286f1972daa06136a504b15cac
pingortle <pingortle@gmail.com>
2013-10-24 14:06:19 -0500

    Add project stubs for data access/domain logic.

---
+++ AdmitOne/AdmitOne.Data.Driver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmitOne.Data.Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Repository();
+        }
+    }
+}
