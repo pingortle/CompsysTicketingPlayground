faf988278671e2713288ea0fed428ccfdcda22dc
pingortle <pingortle@gmail.com>
2013-10-24 12:04:57 -0500

    Add demo functionality to login widget.

--- AdmitOne/AdmitOne/ViewModel/LoginWidgetViewModel.cs
+++ AdmitOne/AdmitOne/ViewModel/LoginWidgetViewModel.cs
@@ -1,19 +1,46 @@
 ﻿using ReactiveUI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reactive.Linq;
 
 namespace AdmitOne.ViewModel
 {
     public sealed class LoginWidgetViewModel : ReactiveObject
     {
-        private ILogPeopleIn _loginManager;
-
         public LoginWidgetViewModel(ILogPeopleIn loginManager)
         {
-            // TODO: Complete member initialization
-            _loginManager = loginManager;
+            Login = new ReactiveCommand(this.WhenAnyValue(x => x.Username, y => y.Password, (x, y) => !string.IsNullOrWhiteSpace(x) && !string.IsNullOrWhiteSpace(y)));
+            Login.RegisterAsyncFunction(_ => loginManager.TryLogin(Username, Password))
+                .Subscribe(x => Status = x ? "Success!" : "Failed.  Please try again.");
+
+            Login.IsExecuting.Where(x => x).Select(_ => "Working...").BindTo(this, x => x.Status);
+
+            _isEnabled = Login.IsExecuting.Select(x => !x).ToProperty(this, x => x.IsEnabled);
         }
+
+        public IReactiveCommand Login { get; private set; }
+
+        private string _username;
+        public string Username
+        {
+            get { return _username ?? ""; }
+            set { this.RaiseAndSetIfChanged(ref _username, value); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password ?? ""; }
+            set { this.RaiseAndSetIfChanged(ref _password, value); }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get { return _status ?? ""; }
+            set { this.RaiseAndSetIfChanged(ref _status, value); }
+        }
+
+        private ObservableAsPropertyHelper<bool> _isEnabled;
+        public bool IsEnabled { get { return _isEnabled.Value; } }
     }
 }
