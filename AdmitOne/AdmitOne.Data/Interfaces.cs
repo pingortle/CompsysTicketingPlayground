03d7c8c99cc906d33bede77b7870e325ab138f5b
pingortle <pingortle@gmail.com>
2013-10-24 14:48:15 -0500

    Add some helpful interfaces.

---
+++ AdmitOne/AdmitOne.Data/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmitOne.Data
+{
+    public interface ITable<T> : IQueryable<T>, IEnumerable<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Update(T item);
+        void Attach(T item);
+    }
+
+    public interface IUnitOfWork<T>
+    {
+        ITable<T> Items { get; }
+        void SaveChanges();
+    }
+}
