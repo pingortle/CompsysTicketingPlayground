36c25759388d0e1bd0c1352f6994324db0a278fd
pingortle <pingortle@gmail.com>
2013-11-01 12:53:38 -0500

    Use ninject as dependency resolver.

--- AdmitOne/AdmitOne/AppBootstrapper.cs
+++ AdmitOne/AdmitOne/AppBootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using AdmitOne.View;
 using AdmitOne.ViewModel;
+using Ninject;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -11,46 +12,46 @@ namespace AdmitOne
 {
     public class AppBootstrapper : ReactiveObject, IScreen
     {
+        // Necessary for construction from App.xaml
         public AppBootstrapper() : this(null, null) { }
 
         public AppBootstrapper(IMutableDependencyResolver dependencyResolver = null, IRoutingState routingState = null)
         {
+            #region Handle Optional Parameters
             Router = routingState ?? new RoutingState();
-            dependencyResolver = dependencyResolver ?? RxApp.MutableResolver;
 
-            RegisterDependencies(dependencyResolver);
+            var kernel = new StandardKernel();
+
+            RxApp.DependencyResolver = dependencyResolver ?? new FuncDependencyResolver((type, contract) => kernel.GetAll(type, contract));
+
+            if (dependencyResolver == null)
+                RxApp.InitializeCustomResolver((obj, type) => kernel.Bind(type).ToConstant(obj));
+            #endregion
+
+            #region Ninject Setup
+            // Singletons
+            kernel.Bind<IScreen>().ToConstant<AppBootstrapper>(this);
+            kernel.Bind<ILogPeopleIn>().To<LoginManager>().InSingletonScope();
+
+            // View resolution
+            kernel.Bind<IViewFor<LoginWidgetViewModel>>().To<LoginWidgetView>();
+            kernel.Bind<IViewFor<MainViewModel>>().To<MainView>();
+            kernel.Bind<IViewFor<CreateTicketsViewModel>>().To<CreateTicketsView>();
+            kernel.Bind<IViewFor<DispatchViewModel>>().To<DispatchView>();
+            kernel.Bind<IViewFor<MyTicketsViewModel>>().To<MyTicketsView>();
+            #endregion
 
             LogHost.Default.Level = LogLevel.Debug;
 
-            LoginWidgetViewModel = new LoginWidgetViewModel(dependencyResolver.GetService<ILogPeopleIn>());
+            LoginWidgetViewModel = kernel.Get<LoginWidgetViewModel>();
 
-            Router.Navigate.Execute(new MainViewModel(dependencyResolver.GetService<IScreen>()));
+            Router.Navigate.Execute(kernel.Get<MainViewModel>());
         }
 
         public LoginWidgetViewModel LoginWidgetViewModel { get; private set; }
 
-        #region Implements IScreen
+        #region IScreen
         public IRoutingState Router { get; private set; }
-        #endregion
-
-        #region Private Configuration Logic
-        // This is where we configure the composition of our components.
-        private void RegisterDependencies(IMutableDependencyResolver dr)
-        {
-            //  Singletons
-            dr.RegisterConstant(this, typeof(IScreen));
-            dr.RegisterLazySingleton(() => new LoginManager(), typeof(ILogPeopleIn));
-            // Views
-            dr.Register(() => new MainView(), typeof(IViewFor<MainViewModel>));
-            dr.Register(() => new LoginWidgetView(), typeof(IViewFor<LoginWidgetViewModel>));
-            dr.Register(() => new CreateTicketsView(), typeof(IViewFor<CreateTicketsViewModel>));
-            dr.Register(() => new DispatchView(), typeof(IViewFor<DispatchViewModel>));
-            dr.Register(() => new MyTicketsView(), typeof(IViewFor<MyTicketsViewModel>));
-            //ViewModels
-            dr.Register(() => new CreateTicketsViewModel(dr.GetService<IScreen>()), typeof(CreateTicketsViewModel));
-            dr.Register(() => new DispatchViewModel(dr.GetService<IScreen>()), typeof(DispatchViewModel));
-            dr.Register(() => new MyTicketsViewModel(dr.GetService<IScreen>()), typeof(MyTicketsViewModel));
-        }
         #endregion
     }
 }
