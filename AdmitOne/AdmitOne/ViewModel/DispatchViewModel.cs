2985ef690cdcb9aec9e7e94360bcdd84eeeecaa5
pingortle <pingortle@gmail.com>
2013-11-05 15:08:22 -0600

    Refactor out unrelated helper method.

--- AdmitOne/AdmitOne/ViewModel/DispatchViewModel.cs
+++ AdmitOne/AdmitOne/ViewModel/DispatchViewModel.cs
@@ -19,46 +19,29 @@ namespace AdmitOne.ViewModel
             Tickets = new ReactiveList<string>();
 
             var getFreshTechs = new ReactiveCommand();
+            getFreshTechs.ObserveOn(RxApp.MainThreadScheduler).Subscribe(_ => Techs.Clear());
             var gotFreshTechs = getFreshTechs.RegisterAsyncFunction(_ => session.GetStoreOf<Employee>().Select(x => x.Name).ToList());
             gotFreshTechs.Subscribe(x => x.ForEach(t => Techs.Add(t)));
 
             var getFreshTickets = new ReactiveCommand();
+            getFreshTickets.ObserveOn(RxApp.MainThreadScheduler).Subscribe(_ => Tickets.Clear());
             var gotFreshTickets = getFreshTickets.RegisterAsyncFunction(_ => session.GetStoreOf<Ticket>().Select(x => x.Description).ToList());
             gotFreshTickets.Subscribe(x => x.ForEach(t => Tickets.Add(t)));
 
             IEnumerable<IReactiveCommand> dataAccessCommands = new[] { getFreshTechs, getFreshTickets };
-            var masterCommand = JoinMutuallyExclusiveAsyncCommands(
-                dataAccessCommands,
-                new[] { gotFreshTechs, gotFreshTickets });
+            var masterCommand = dataAccessCommands.JoinMutuallyExclusiveAsyncCommands(new[]
+            {
+                gotFreshTechs,
+                gotFreshTickets
+            });
 
             Refresh = masterCommand;
-            Refresh.Subscribe(_ => { Techs.Clear(); Tickets.Clear(); });
 
             _error = Observable.Merge(dataAccessCommands.Select(x => x.ThrownExceptions))
                 .Select(x => x.Message)
                 .ToProperty(this, x => x.Error);
 
             masterCommand.Execute(default(object));
-        }
-
-        public IReactiveCommand JoinMutuallyExclusiveAsyncCommands(IEnumerable<IReactiveCommand> commands, IEnumerable<IObservable<object>> asyncResults)
-        {
-            if (!commands.Any() || !asyncResults.Any()) throw new ArgumentException("Enumerables must have elements.");
-            if (commands.Count() != asyncResults.Count()) throw new ArgumentException("Enumerables must have equal numbers of elements.");
-
-            var masterCommand = new ReactiveCommand(Observable.Merge(commands.Select(x => x.CanExecuteObservable)));
-
-            masterCommand.RegisterAsyncAction(x =>
-            {
-                commands.Zip(asyncResults, (n, m) => new { Command = n, ObservableResult = m }).Aggregate((a, b) =>
-                    {
-                        b.ObservableResult.Take(1).Subscribe(_ => a.Command.Execute(x));
-                        return b;
-                    })
-                    .Command.Execute(x);
-            });
-
-            return masterCommand;
         }
 
         public IReactiveCollection<string> Techs { get; private set; }
