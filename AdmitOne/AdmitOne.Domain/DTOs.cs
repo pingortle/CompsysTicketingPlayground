cc3152d9d7b604d1646e5d9213b7fba1395b50e9
pingortle <pingortle@gmail.com>
2013-11-05 14:55:52 -0600

    Add some entities.
    **SQL BREAKING CHANGE**

--- AdmitOne/AdmitOne.Domain/DTOs.cs
+++ AdmitOne/AdmitOne.Domain/DTOs.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AdmitOne.Domain
 {
+    public enum TicketStatus { Open, Closed, Assigned }
+
     public class Customer
     {
         public int Id { get; set; }
@@ -13,6 +16,15 @@ namespace AdmitOne.Domain
         public virtual List<Ticket> Tickets { get; set; }
     }
 
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual List<TicketEvent> TicketEvents { get; set; }
+    }
+
     public class Ticket
     {
         public int Id { get; set; }
@@ -20,7 +32,22 @@ namespace AdmitOne.Domain
         public string Description { get; set; }
 
         public int CustomerId { get; set; }
-
         public virtual Customer Customer { get; set; }
+
+        public virtual List<TicketEvent> TicketEvents { get; set; }
+    }
+
+    public class TicketEvent
+    {
+        public int Id { get; set; }
+
+        public TicketStatus? TicketStatus { get; set; }
+        public DateTime Time { get; set; }
+
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
     }
 }
