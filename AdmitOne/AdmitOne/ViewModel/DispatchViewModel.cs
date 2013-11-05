c5563cdc2cb395885bee0f68a8b62d1fc83735ce
pingortle <pingortle@gmail.com>
2013-11-05 14:57:53 -0600

    Load dispatcher screen from persistent store.

--- AdmitOne/AdmitOne/ViewModel/DispatchViewModel.cs
+++ AdmitOne/AdmitOne/ViewModel/DispatchViewModel.cs
@@ -1,21 +1,67 @@
-﻿using ReactiveUI;
+﻿using AdmitOne.Domain;
+using AdmitOne.Persistence;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
 
 namespace AdmitOne.ViewModel
 {
     public sealed class DispatchViewModel : ReactiveObject, IRoutableViewModel
     {
-        public DispatchViewModel(IScreen screen)
+        public DispatchViewModel(IScreen screen, ISession session)
         {
             HostScreen = screen;
             GoBack = HostScreen.Router.NavigateBack;
+
+            Techs = new ReactiveList<string>();
+            Tickets = new ReactiveList<string>();
+
+            var getFreshTechs = new ReactiveCommand();
+            var gotFreshTechs = getFreshTechs.RegisterAsyncFunction(_ => session.GetStoreOf<Employee>().Select(x => x.Name).ToList());
+            gotFreshTechs.Subscribe(x => x.ForEach(t => Techs.Add(t)));
+
+            var getFreshTickets = new ReactiveCommand();
+            var gotFreshTickets = getFreshTickets.RegisterAsyncFunction(_ => session.GetStoreOf<Ticket>().Select(x => x.Description).ToList());
+            gotFreshTickets.Subscribe(x => x.ForEach(t => Tickets.Add(t)));
+
+            IEnumerable<IReactiveCommand> dataAccessCommands = new[] { getFreshTechs, getFreshTickets };
+            var masterCommand = JoinMutuallyExclusiveAsyncCommands(
+                dataAccessCommands,
+                new[] { gotFreshTechs, gotFreshTickets });
+
+            Refresh = masterCommand;
+            Refresh.Subscribe(_ => { Techs.Clear(); Tickets.Clear(); });
+
+            masterCommand.Execute(default(object));
         }
 
-#warning Demo code!
-        public IEnumerable<string> Techs { get { return new[] { "Tech1", "Tech2", "Tech2", "Tech2", "Tech2", "Tech2", "Tech2", "Tech2", }; } }
-        public IEnumerable<string> Tickets { get { return new[] { "Ticket1", "Ticket2", "Ticket2", "Ticket2", "Ticket2", "Ticket2", "Ticket2", "Ticket2", }; } }
+        public IReactiveCommand JoinMutuallyExclusiveAsyncCommands(IEnumerable<IReactiveCommand> commands, IEnumerable<IObservable<object>> asyncResults)
+        {
+            if (!commands.Any() || !asyncResults.Any()) throw new ArgumentException("Enumerables must have elements.");
+            if (commands.Count() != asyncResults.Count()) throw new ArgumentException("Enumerables must have equal numbers of elements.");
+
+            var masterCommand = new ReactiveCommand(Observable.Merge(commands.Select(x => x.CanExecuteObservable)));
+
+            masterCommand.RegisterAsyncAction(x =>
+            {
+                commands.Zip(asyncResults, (n, m) => new { Command = n, ObservableResult = m }).Aggregate((a, b) =>
+                    {
+                        b.ObservableResult.Take(1).Subscribe(_ => a.Command.Execute(x));
+                        return b;
+                    })
+                    .Command.Execute(x);
+            });
+
+            return masterCommand;
+        }
+
+        public IReactiveCollection<string> Techs { get; private set; }
+        public IReactiveCollection<string> Tickets { get; private set; }
 
         public IReactiveCommand GoBack { get; private set; }
+        public IReactiveCommand Refresh { get; private set; }
 
         public IScreen HostScreen { get; private set; }
 
