d50d8f709fbe248c041fb22aaabfd44edb38bbec
pingortle <pingortle@gmail.com>
2013-11-12 16:02:15 -0600

    Create tentative dto pattern.

---
+++ AdmitOne/AdmitOne.Domain/DataTransferObjects.cs
@@ -0,0 +1,66 @@
+﻿using AdmitOne.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace AdmitOne.Domain
+{
+    public class TicketWithLatestEvent : ITicket, ITicketEvent
+    {
+        #region Generated Code
+        public string Description
+        {
+            get; set;
+        }
+
+        public int CustomerId
+        {
+            get; set;
+        }
+
+        public Customer Customer
+        {
+            get; set;
+        }
+
+        public List<TicketEvent> TicketEvents
+        {
+            get; set;
+        }
+
+        public int Id
+        {
+            get; set;
+        }
+
+        public TicketStatus? TicketStatus
+        {
+            get; set;
+        }
+
+        public DateTime Time
+        {
+            get; set;
+        }
+
+        public int EmployeeId
+        {
+            get; set;
+        }
+
+        public Employee Employee
+        {
+            get; set;
+        }
+
+        public int TicketId
+        {
+            get; set;
+        }
+
+        public Ticket Ticket
+        {
+            get; set;
+        }
+        #endregion
+    }
+}
